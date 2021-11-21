7b22a670758565d901d3be5149877a6fe2e34fe8
UTkraken <48678025+UTkraken@users.noreply.github.com>
2021-11-21 17:15:01 +0100

    add select all

--- ceresia_wep_app/Ceresia/views.py
+++ ceresia_wep_app/Ceresia/views.py
@@ -1,14 +1,27 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+
+from Ceresia.models import Hike, History, Species
 
 
 def hikes(request):
-    return render(request, 'ceresia/hikes.html')
+    hikes = Hike.objects.all()
+    contexte = {
+        'hikes': hikes
+    }
+    return render(request, 'ceresia/hikes.html', contexte)
 
 
 def history(request):
+    history = History.objects.all()
+    contexte = {
+        'history': history
+    }
     return render(request, 'ceresia/history.html')
 
 
 def cerescope(request):
+    species = Species.objects.all()
+    contexte = {
+        'species': species
+    }
     return render(request, 'ceresia/cerescope.html')
